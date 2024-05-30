79e1e1a039e67a055b813dd30fe5153a5e6f2d5f
yumanuralfath <yumanuralfath2@gmail.com>
2024-05-30 14:00:37 +0700

    feat: add 13-15 test

---
+++ CODEWARS/8 KYU/12.ts
@@ -0,0 +1,5 @@
+const numberToString = (num: number): string => {
+  return num.toString();
+};
+
+console.log(numberToString(2007));
---
+++ CODEWARS/8 KYU/13.ts
@@ -0,0 +1,7 @@
+export class Kata {
+  static opposite(number: number): number {
+    return number * -1;
+  }
+}
+
+console.log(Kata.opposite(20));
